d43782e3f5c207bf1d8a8687d269fea931c2b646
Astromium <interstellarspace2000@gmail.com>
2020-10-10 10:00:28 +0100

    removed the slider library and implemented it with vanilla js

--- public/js/project.js
+++ public/js/project.js
@@ -1,39 +1,43 @@
 const showToast = (message, gr, pos, bgColor) => {
-    Toastify({
-        text: message,
-        duration: 3000,
-        newWindow: true,
-        //close: true,
-        gravity: gr, // `top` or `bottom`
-        position: pos, // `left`, `center` or `right`
-        backgroundColor: bgColor,
-        stopOnFocus: true, // Prevents dismissing of toast on hover
-        className: 'toast',
-    }).showToast();
+  Toastify({
+    text: message,
+    duration: 3000,
+    newWindow: true,
+    //close: true,
+    gravity: gr, // `top` or `bottom`
+    position: pos, // `left`, `center` or `right`
+    backgroundColor: bgColor,
+    stopOnFocus: true, // Prevents dismissing of toast on hover
+    className: 'toast',
+  }).showToast();
 };
 
-const deleteBtn = document.getElementById('delete-btn')
+const deleteBtn = document.getElementById('delete-btn');
 deleteBtn.addEventListener('click', () => {
-    const deleteContainer = document.getElementById('confirm-delete-container').style.opacity = 1
-})
+  const deleteContainer = (document.getElementById(
+    'confirm-delete-container'
+  ).style.display = 'flex');
+});
 
-const cancel = document.getElementById('cancel')
+const cancel = document.getElementById('cancel');
 cancel.addEventListener('click', () => {
-    const deleteContainer = document.getElementById('confirm-delete-container').style.opacity = 0
-})
+  const deleteContainer = (document.getElementById(
+    'confirm-delete-container'
+  ).style.display = 'none');
+});
 
 const handleDelete = async (id) => {
-    try {
-        const res = await axios({
-            method: 'DELETE',
-            url: `http://127.0.0.1:3000/api/v1/projects/${id}`
-        })
-        if (res.data.status === 'success') {
-            showToast('Project Deleted', 'bottom', 'right', '#1da977')
-            setTimeout(() => location.assign('/me'), 1500)
-        }
-    } catch (err) {
-        showToast('Something went wrong', 'bottom', 'right', '#1da977')
-        console.log(err);
+  try {
+    const res = await axios({
+      method: 'DELETE',
+      url: `http://127.0.0.1:3000/api/v1/projects/${id}`,
+    });
+    if (res.data.status === 'success') {
+      showToast('Project Deleted', 'bottom', 'right', '#1da977');
+      setTimeout(() => location.assign('/me'), 1500);
     }
-}
+  } catch (err) {
+    showToast('Something went wrong', 'bottom', 'right', '#1da977');
+    console.log(err);
+  }
+};
